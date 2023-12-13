2ec12a83ece1c0a97271e5d390cb8b4049f01089
harodggg <haroldtiansheng@gmail.com>
2023-12-13 20:48:01 +0800

    Feat: add install_ansible.sh scirpt

---
+++ ansible/install-ansible.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+command -v ansible;
+
+if [ $? -eq 1 ];then
+    echo "ansible command is not exsit"
+    sudo apt-get -y update 
+    sudo apt-get install -y software-properties-common 
+    sudo apt-add-repository -y ppa:ansible/ansible 
+    sudo apt-get -y update 
+    sudo apt-get -y install ansible 
+    else
+    echo "ansible is installed"
+fi
