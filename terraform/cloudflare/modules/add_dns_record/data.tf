950274550c14b1517ff6cf1f8b7ae71bec1bdf67
harodggg <haroldtiansheng@gmail.com>
2024-02-05 11:33:34 +0800

    feat: impove ansible
    Signed-off-by: harodggg <haroldtiansheng@gmail.com>

--- terraform/cloudflare/modules/add_dns_record/data.tf
+++ terraform/cloudflare/modules/add_dns_record/data.tf
@@ -4,7 +4,7 @@ data "cloudflare_zone" "zone" {
 
  data "cloudflare_record" "domain_record" {
     zone_id  = data.cloudflare_zone.zone.id
-    hostname = "${var.domain_name}.blocknode.run"
+    hostname = "${var.domain_name}.${var.domain}"
     depends_on = [
     # Security group rule must be created before this IP address could
     # actually be used, otherwise the services will be unreachable.
